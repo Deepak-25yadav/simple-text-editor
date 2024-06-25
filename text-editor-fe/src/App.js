e351e4a18b021dceeb98fa6df4bba0d26762dc6a
Deepak-25yadav <25yadavdeepak@gmail.com>
2024-06-26 03:54:44 +0530

    text-editor-done

--- text-editor-fe/src/App.js
+++ text-editor-fe/src/App.js
@@ -1,11 +1,26 @@
-// import logo from './logo.svg';
+
+
+// src/App.js
+
+import React from 'react';
+import Editor from './components/Editor';
+import FontSelector from './components/FontSelector';
+import WeightSelector from './components/WeightSelector';
+import ItalicToggle from './components/ItalicToggle';
+import { EditorProvider } from './context/EditorContext';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <EditorProvider>
+      <div className="App">
+        <h1>Text Editor</h1>
+        <FontSelector />
+        <WeightSelector />
+        <ItalicToggle />
+        <Editor />
+      </div>
+    </EditorProvider>
   );
 }
 
